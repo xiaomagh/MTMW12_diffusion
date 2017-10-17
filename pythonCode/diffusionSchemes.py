f530be26cfb34c49b0473dc8b54df33373734d48
Xiao Ma <zq806676@live.reading.ac.uk>
2017-10-17 15:33:46 +0100

    WEEK4

---
+++ pythonCode/diffusionSchemes.py
@@ -0,0 +1,8 @@
+# Numerical schemes for simulating diffusion for outer code diffusion.py
+
+from __future__ import absolute_import, division, print_function
+import numpy as np
+
+# The linear algebra package for BTCS (for solving the matrix equation)
+import scipy.linalg as la
+
